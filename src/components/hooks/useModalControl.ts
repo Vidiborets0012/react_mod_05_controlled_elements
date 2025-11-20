1500a340cadb061d9983a6c7cd45e5b30a9d4cad
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-20 22:02:52 +0200

    type useModalConrol

--- src/components/hooks/useModalControl.ts
+++ src/components/hooks/useModalControl.ts
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
-export default function useModalConrol() {
+export default function useModalConrol(): {
+  isModalOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+} {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
